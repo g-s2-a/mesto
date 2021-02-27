05f7b0827b2ff0b3bd481c6e0438fb8ee13ff6d3
Andrey <s2-a@yandex.ru>
2021-02-27 21:40:37 +0500

    Проектная работа 4

---
+++ js/popup.js
@@ -0,0 +1,30 @@
+let showPB = document.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let closePB = document.querySelector(".popup__close");
+let formElement = document.querySelector(".popup__content");
+let nameInput = document.querySelector("#popup__name");
+let jobInput = document.querySelector("#popup__profession");
+let profileTitle = document.querySelector(".profile__title");
+let profileKind = document.querySelector(".profile__kind-of-activity");
+
+showPB.addEventListener("click",togglePopup);
+closePB.addEventListener("click",togglePopup);
+popup.addEventListener("click",togglePopup);
+
+formElement.addEventListener("click",function (event){ 
+    event.stopPropagation(); // останавливает распространиение события клика на родителей элемента  
+});
+
+formElement.addEventListener("submit", formSubmitHandler);
+
+function formSubmitHandler(event){
+    event.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileKind.textContent = jobInput.value; 
+    togglePopup();  
+};
+
+function togglePopup(event){
+    //event.preventDefault();
+    popup.classList.toggle('popup_open'); 
+};
